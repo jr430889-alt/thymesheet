37b7eb8a31c480d8551e0500d4bd99fd85e46f97
docrocket-mad <devan@madsciencemaritimes.com>
2025-09-15 22:50:53 -0300

    Jeremy's Time Tracker

---
+++ START_TIME_TRACKER.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting Jeremy's Time Tracker...
+echo.
+echo First time setup:
+echo 1. Installing dependencies...
+call npm install
+echo.
+echo 2. Starting the application...
+call npm start
+pause
---
+++ main.js
@@ -0,0 +1,129 @@
+const { app, BrowserWindow, Tray, Menu, nativeImage, globalShortcut } = require('electron');
+const path = require('path');
+
+let mainWindow;
+let tray;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    show: false
+  });
+
+  mainWindow.loadFile('Grindstone-2.0.html');
+
+  mainWindow.on('close', (event) => {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+}
+
+function createTray() {
+  // Create a simple 16x16 black square as tray icon
+  const icon = nativeImage.createEmpty();
+
+  // Create a simple icon programmatically
+  const canvas = {
+    width: 16,
+    height: 16,
+    data: Buffer.alloc(16 * 16 * 4) // RGBA
+  };
+
+  // Fill with dark pixels to make it visible
+  for (let i = 0; i < canvas.data.length; i += 4) {
+    canvas.data[i] = 100;     // R
+    canvas.data[i + 1] = 100; // G
+    canvas.data[i + 2] = 100; // B
+    canvas.data[i + 3] = 255; // A
+  }
+
+  const trayIcon = nativeImage.createFromBitmap(canvas.data, { width: 16, height: 16 });
+  tray = new Tray(trayIcon);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show Time Tracker',
+      click: () => {
+        mainWindow.show();
+      }
+    },
+    {
+      label: 'Start/Stop Timer',
+      click: () => {
+        // Send a message to the renderer process to toggle timer
+        mainWindow.webContents.send('toggle-timer');
+      }
+    },
+    { type: 'separator' },
+    {
+      label: 'Quit',
+      click: () => {
+        app.isQuiting = true;
+        app.quit();
+      }
+    }
+  ]);
+
+  tray.setToolTip('Grindstone Time Tracker');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('double-click', () => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  createTray();
+
+  // Register global hotkeys
+  const ret = globalShortcut.register('CommandOrControl+Shift+T', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+      mainWindow.focus();
+    }
+  });
+
+  const ret2 = globalShortcut.register('CommandOrControl+Shift+Space', () => {
+    // Toggle timer with hotkey
+    mainWindow.webContents.send('toggle-timer');
+  });
+
+  if (!ret) {
+    console.log('Registration failed for Ctrl+Shift+T');
+  }
+
+  if (!ret2) {
+    console.log('Registration failed for Ctrl+Shift+Space');
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('will-quit', () => {
+  // Unregister all shortcuts
+  globalShortcut.unregisterAll();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
